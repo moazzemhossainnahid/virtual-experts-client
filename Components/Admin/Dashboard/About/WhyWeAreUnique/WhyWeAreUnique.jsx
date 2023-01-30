e32b23e44bd62de6b059df8de6c0efb94816bc6c
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-01-30 20:59:07 +0600

    why unique card design

--- Components/Admin/Dashboard/About/WhyWeAreUnique/WhyWeAreUnique.jsx
+++ Components/Admin/Dashboard/About/WhyWeAreUnique/WhyWeAreUnique.jsx
@@ -16,9 +16,9 @@ const AdminWhyWeAreUnique = () => {
     loadData();
   }, [number]);
   return (
-    <section className="my-2 boxShadow me-3 p-3">
-      <h1 className="fs-24">Edit Why We are unique Section</h1>
-      <h6 className="fs-18">Answers</h6>
+    <section className="my-2 boxShadow bg-gray rounded me-3 p-3">
+      <h1 className="fs-24 spacing-3 text-center py-3">Why We are unique Section</h1>
+      {/* <h6 className="fs-18">Answers</h6> */}
       {answers.map((answer, index) => (
         <WhyWeAreUniqueSinglData
           key={answer._id}
