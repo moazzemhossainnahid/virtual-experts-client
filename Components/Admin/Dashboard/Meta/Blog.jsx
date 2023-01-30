28d6c94c59c2fa47cc57ebad7f951daa679497fc
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-01-30 21:42:00 +0600

    meta blog add

--- Components/Admin/Dashboard/Meta/Blog.jsx
+++ Components/Admin/Dashboard/Meta/Blog.jsx
@@ -4,104 +4,106 @@ import { AiFillEdit } from "react-icons/ai";
 import { toast } from "react-toastify";
 
 const Blog = ({ blogData, setNumber }) => {
-  const { _id, title, description } = blogData;
-  const newTitle = title;
-  const newDescription = description;
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    const title = data.title || newTitle;
-    const description = data.description || newDescription;
+    const { _id, title, description } = blogData;
+    const newTitle = title;
+    const newDescription = description;
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        const title = data.title || newTitle;
+        const description = data.description || newDescription;
 
-    fetch("http://localhost:5000/metaBlog/update", {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ _id, title, description }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        toast.success("Updates Successfully");
-        setNumber((preState) => preState + 1);
-      });
-  };
+        fetch("http://localhost:5000/metaBlog/update", {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ _id, title, description }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                toast.success("Updates Successfully");
+                setNumber((preState) => preState + 1);
+            });
+    };
 
-  return (
-    <>
-      <section className="my-2 boxShadow me-3 p-3">
-        <div className="d-flex justify-content-between">
-          <h1 className="fs-24">Blog Page</h1>
-          <AiFillEdit
-            size={24}
-            className="text-warning cursor-pointer "
-            data-bs-toggle="modal"
-            data-bs-target="#blogModal"
-          />
-        </div>
-        <h6 className="fs-18 mt-2">Title</h6>
-        <p className="fs-14">{title}</p>
-        <h6 className="fs-18 mt-2">Description</h6>
-        <p className="fs-14">{description}</p>
-      </section>
+    return (
+        <>
+            <section className="my-2 boxShadow bg-gray rounded me-3 p-3">
+                <div className="d-flex justify-content-center">
+                    <h1 className="fs-24 spacing-3">Blog Page</h1>
+                </div>
+                <h6 className="fs-18 mt-2">Title</h6>
+                <p className="fs-14">{title}</p>
+                <h6 className="fs-18 mt-2">Description</h6>
+                <p className="fs-14">{description}</p>
+                <div className="d-flex py-3 justify-content-end">
+                    <AiFillEdit
+                        size={30}
+                        className="text-warning bg-dark rounded-circle p-1 cursor-pointer "
+                        data-bs-toggle="modal"
+                        data-bs-target="#blogModal"
+                    />
+                </div>
+            </section>
 
-      {/* =============== modal */}
+            {/* =============== modal */}
 
-      <div
-        className="modal fade"
-        id="blogModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Meta Tag and description For Blog
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
+            <div
+                className="modal fade"
+                id="blogModal"
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">
+                                Meta Tag and description For Blog
+                            </h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <label htmlFor="title">Meta Title</label>
+                                <textarea
+                                    rows="2"
+                                    cols="2"
+                                    defaultValue={title}
+                                    {...register("title")}
+                                    name="title"
+                                    id="title"
+                                    className="form-control mb-2"
+                                ></textarea>
+                                <label htmlFor="title">Meta description</label>
+                                <textarea
+                                    rows="2"
+                                    cols="2"
+                                    defaultValue={description}
+                                    {...register("description")}
+                                    name="description"
+                                    id="description"
+                                    className="form-control mb-2"
+                                ></textarea>
+
+                                <input
+                                    type="submit"
+                                    className="btn btn-primary"
+                                    value="Save Changes"
+                                    data-bs-dismiss="modal"
+                                />
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="modal-body">
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="title">Meta Title</label>
-                <textarea
-                  rows="2"
-                  cols="2"
-                  defaultValue={title}
-                  {...register("title")}
-                  name="title"
-                  id="title"
-                  className="form-control mb-2"
-                ></textarea>
-                <label htmlFor="title">Meta description</label>
-                <textarea
-                  rows="2"
-                  cols="2"
-                  defaultValue={description}
-                  {...register("description")}
-                  name="description"
-                  id="description"
-                  className="form-control mb-2"
-                ></textarea>
-
-                <input
-                  type="submit"
-                  className="btn btn-primary"
-                  value="Save Changes"
-                  data-bs-dismiss="modal"
-                />
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Blog;
