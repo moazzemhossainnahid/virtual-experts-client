13158428dcbab77f234da500a722000326ec2944
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-01-31 23:45:15 +0600

    add cform sidebar

--- Components/Admin/Dashboard/Others/Sidebar/Sidebar.jsx
+++ Components/Admin/Dashboard/Others/Sidebar/Sidebar.jsx
@@ -93,6 +93,15 @@ const Sidebar = () => {
           
         </Link> */}
         <Link
+          href="/richard/cform"
+          className={`d-block py-2 ${
+            pathname === "/richard/cform" ? "bg-orange" : ""
+          } px-3 rounded-3 text-white my-2 navHover`}>
+          
+            Contact
+          
+        </Link>
+        <Link
           href="/richard/footer"
           className={`d-block py-2 ${
             pathname === "/richard/footer" ? "bg-orange" : ""
