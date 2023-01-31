c2ea656d3b0dccb7542c0beb711a115e3f360ecf
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-01-31 15:22:00 +0600

    why should hire call

---
+++ Components/Admin/Dashboard/FBA/WhyShouldHireFBA/WhyShouldHireFBA.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import FBAServiceSingleData from "./FBAServiceSingleData";
+
+const AdminWhyShouldHireFBA = () => {
+  const [number, setNumber] = useState(0);
+  const [answers, setAnswers] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const whyShouldHireResponse = await fetch(
+        "http://localhost:5000/whyshouldhire"
+      );
+      const whyShouldHireData = await whyShouldHireResponse.json();
+      setAnswers(whyShouldHireData);
+    };
+    loadData();
+  }, [number]);
+  return (
+    <section className="my-2 boxShadow bg-gray rounded me-3 p-3">
+      <h1 className="fs-24 spacing-3 text-center py-3">Why Should Hire FBA</h1>
+      {/* <h6 className="fs-18">Answers</h6> */}
+      {answers?.map((answer, index) => (
+        <FBAServiceSingleData
+          key={answer._id}
+          answer={answer}
+          index={index}
+          setNumber={setNumber}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default AdminWhyShouldHireFBA;
