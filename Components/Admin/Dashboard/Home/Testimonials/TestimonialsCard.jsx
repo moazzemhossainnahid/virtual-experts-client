f1b75b3ca3f04f6fa5b4413329317856e4db2b4d
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-02-06 01:37:16 +0600

    testimonial delete modal add

--- Components/Admin/Dashboard/Home/Testimonials/TestimonialsCard.jsx
+++ Components/Admin/Dashboard/Home/Testimonials/TestimonialsCard.jsx
@@ -180,6 +180,53 @@ const TestimonialsCard = ({ testimonial, testimonials, setTestimonials, index, s
           </div>
         </div>
       </div>
+
+      {/* Modal end here */}
+      {/* For Delete Card data Start */}
+      <div
+        className="modal fade"
+        id={`cardDelete${index + 1}`}
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                DELETE - {testimonial.name}
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p>Are you want to delete this?</p>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-bs-dismiss="modal"
+                onClick={() => deleteTestimonial(testimonial._id)}
+              >
+                Yes
+              </button>
+              <button
+                type="button"
+                className="btn btn-success"
+                data-bs-dismiss="modal"
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* For Delete Card data Finish */}
     </>
   );
 };
