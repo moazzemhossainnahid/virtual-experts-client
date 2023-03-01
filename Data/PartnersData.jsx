a3ff65e569c61dbf73989815651fa00278b3f58f
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-01 23:20:59 +0600

    design slide

--- Data/PartnersData.jsx
+++ Data/PartnersData.jsx
@@ -17,4 +17,40 @@ export const PartnersData = [
     {
         imageURL: 'https://i.ibb.co/jz5tZ7r/keeva.png',
     },
+    {
+        imageURL: 'https://i.ibb.co/Tg1wwjx/amcrest.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/SxrChkV/Anker.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/khcSQQG/aspen.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/bXJV1vT/Foscam.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/T03CbcY/HS.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/jz5tZ7r/keeva.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/Tg1wwjx/amcrest.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/SxrChkV/Anker.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/khcSQQG/aspen.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/bXJV1vT/Foscam.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/T03CbcY/HS.png',
+    },
+    {
+        imageURL: 'https://i.ibb.co/jz5tZ7r/keeva.png',
+    },
 ];
