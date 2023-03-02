b23996c0efeab78ef8459b18a45a601e115e5789
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-02 23:21:25 +0600

    sec devide

--- Components/Services/BestProduct.jsx
+++ Components/Services/BestProduct.jsx
@@ -13,10 +13,10 @@ const BestProduct = () => {
                 <div style={{ height: '80px', backgroundColor: "#000000" }} className=" d-flex align-items-center justify-content-center col-12 mx-auto mb-3 text-center">
                     <h2 style={{ letterSpacing: '2px' }} className="text-light fw-bold text-uppercase fs-30">Our Top Selling Product</h2>
                 </div>
-                <div className="">
-                    <div style={{marginTop:"-30px"}} className="d-flex justify-content-left align-items-center ">
+                <div className="w-full position-relative">
+                    <div style={{ marginTop: "-30px" }} className="d-flex justify-content-left align-items-center ">
                         <Image
-                            style={{zIndex:"5"}}
+                            style={{ zIndex: "5" }}
                             src={`https://i.ibb.co/gPPFLNS/no1.png`}
                             alt="no-1"
                             title="no-1"
@@ -25,6 +25,17 @@ const BestProduct = () => {
                         />
                         <div style={{ backgroundImage: "url('https://i.ibb.co/3Tzr0bh/Seek-Png-com-orange-banner.png')", backgroundRepeat: "no-repeat", marginLeft: "-10px", zIndex: "1" }} className="px-3 py-1 text-light">
                             Best in Test 2023
+                        </div>
+                    </div>
+                    <div style={{marginTop:"-30px"}} className="d-flex flex-column flex-md-row row justify-content-center bg-warning align-items-center">
+                        <div className="col-12 col-md-3 bg-danger">
+                            <h2 className="">hey there</h2>
+                        </div>
+                        <div className="col-12 col-md-6 bg-dark">
+                            <h2 className="">hey there</h2>
+                        </div>
+                        <div className="col-12 col-md-3 bg-info">
+                            <h2 className="">hey there</h2>
                         </div>
                     </div>
                 </div>
