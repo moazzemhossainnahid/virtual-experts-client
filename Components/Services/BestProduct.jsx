344abd08df3477cb1ef6c7d2e8213054ffdc3076
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-02 22:29:38 +0600

    no1 add

--- Components/Services/BestProduct.jsx
+++ Components/Services/BestProduct.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import ReactHtmlParser from "react-html-parser";
@@ -5,14 +6,27 @@ import ReactHtmlParser from "react-html-parser";
 const BestProduct = () => {
 
 
+
     return <>
         <div className="container my-5 pb-3 px-md-5">
             <div className="background-color-skyblue borderRadius">
                 <div style={{ height: '80px', backgroundColor: "#000000" }} className=" d-flex align-items-center justify-content-center col-12 mx-auto mb-3 text-center">
-                    <h2 style={{letterSpacing:'2px'}} className="text-light fw-bold text-uppercase fs-30">Our Top Selling Product</h2>
+                    <h2 style={{ letterSpacing: '2px' }} className="text-light fw-bold text-uppercase fs-30">Our Top Selling Product</h2>
                 </div>
                 <div className="">
-                    
+                    <div style={{marginTop:"-30px"}} className="d-flex justify-content-left align-items-center ">
+                        <Image
+                            style={{zIndex:"5"}}
+                            src={`https://i.ibb.co/gPPFLNS/no1.png`}
+                            alt="no-1"
+                            title="no-1"
+                            width={50}
+                            height={50}
+                        />
+                        <div style={{ backgroundImage: "url('https://i.ibb.co/3Tzr0bh/Seek-Png-com-orange-banner.png')", backgroundRepeat: "no-repeat", marginLeft: "-10px", zIndex: "1" }} className="px-3 py-1 text-light">
+                            Best in Test 2023
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
