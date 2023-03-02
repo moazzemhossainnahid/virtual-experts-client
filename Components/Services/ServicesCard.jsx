9179f11cca3ac85864cebd6f4577ede3436ce6ef
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-02 11:26:55 +0600

    card redesign

--- Components/Services/ServicesCard.jsx
+++ Components/Services/ServicesCard.jsx
@@ -28,7 +28,7 @@ const ServicesCard = ({ servicesCardData, serviceCardHeader }) => {
                                 key={servicesCard._id}
                             >
                                 <div className="card h-100 border-0 borderRadius overflow-hidden">
-                                    <div className="background-color-skyblue overflow-hidden">
+                                    <div className="background-color-skyblue overflow-hidden ">
                                         {servicesCard.img ? (
                                             <Image
                                                 src={`${imgType} ; base64, ${servicesCard.img.img}`}
@@ -50,61 +50,63 @@ const ServicesCard = ({ servicesCardData, serviceCardHeader }) => {
                                     </div>
                                     <div className="card-body">
                                         <div style={{ height: "300px" }} className="">
-                                            <h3 style={{ height: "55px" }} className="fs-18 lh-28 mb-4 roboto-font-family fw-normal h-">
+                                            <h3 style={{ height: "50px" }} className="fs-18 lh-28 mb-4 fw-bold roboto-font-family fw-normal h-">
                                                 {servicesCard.title}
                                             </h3>
-                                            <p className="fs-14 lh-26 font-medium m-0">
+                                            <p className="fs-14 lh-24 font-medium m-0 text-justify">
                                                 {servicesCard.subTitle}
                                             </p>
-                                            {servicesCard.regularReview && (
+                                            {/* {servicesCard.regularReview && (
                                                 <p className="fs-14 m-0 lh-26">
                                                     Regular Review : ${servicesCard.regularReview} each
                                                 </p>
-                                            )}
+                                            )} */}
                                             {/* {servicesCard.videoReview && (
                                                 <p className="fs-14 m-0 lh-26">
                                                     Video Review : ${servicesCard.videoReview} each
                                                 </p>
-                                            )}
-                                            {servicesCard.top50Reviewers && (
+                                            )} */}
+                                            {/* {servicesCard.top50Reviewers && (
                                                 <p className="fs-14 m-0 lh-26">
                                                     Top 50 Reviewers : ${servicesCard.top50Reviewers} each
                                                 </p>
-                                            )}
+                                            )} */}
                                             {servicesCard.price && (
                                                 <p className="fs-14 m-0 lh-26">
                                                     Price : {servicesCard.price}
                                                 </p>
                                             )}
-                                            {servicesCard.delivery && (
+                                            {/* {servicesCard.delivery && (
                                                 <p className="fs-14 m-0 lh-26">
                                                     Delivery : {servicesCard.delivery} days
                                                 </p>
-                                            )}
-                                            {servicesCard.warranty && (
+                                            )} */}
+                                            {/* {servicesCard.warranty && (
                                                 <p className="fs-14 lh-26">
                                                     Warranty : {servicesCard.warranty}
                                                 </p>
-                                            )}
-                                            {servicesCard.maintenance && (
+                                            )} */}
+                                            {/* {servicesCard.maintenance && (
                                                 <p className="fs-14 lh-26">
                                                     Maintenence : ${servicesCard.maintenance} per month
                                                 </p>
                                             )} */}
                                         </div>
-                                        <Link href="/order" scroll>
+                                        <div className="d-flex justify-content-center">
+                                            <Link href="/order" scroll>
 
-                                            <button
-                                                className="button px-4 py-1 slide_right mt-3"
-                                                style={{
-                                                    padding: "5px 20px",
-                                                    backgroundColor: "white",
-                                                }}
-                                            >
-                                                <h6 className="fs-14 d-inline">Order Now</h6>
-                                            </button>
+                                                <button
+                                                    className="button px-4 py-1 slide_right mt-3"
+                                                    style={{
+                                                        padding: "5px 20px",
+                                                        backgroundColor: "white",
+                                                    }}
+                                                >
+                                                    <h6 className="fs-14 d-inline">Order Now</h6>
+                                                </button>
 
-                                        </Link>
+                                            </Link>
+                                        </div>
                                     </div>
                                     <div className="position-absolute end-0 card-shape-bottom ">
                                         <Image src={shape} alt="shape" height="120" width="120" />
