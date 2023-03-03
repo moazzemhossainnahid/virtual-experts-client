4e77def8cb90731121ac2df6f2b99b800b959b7e
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-03-03 21:40:05 +0600

    desktop banner add

--- Components/Services/BestProduct.jsx
+++ Components/Services/BestProduct.jsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import ReactHtmlParser from "react-html-parser";
+import angle from "../../Assets/Images/others/Rectangle 266.svg";
+import ring from "../../Assets/Images/service/Ellipse 261.svg";
 
 const BestProduct = () => {
 
 
 
     return <>
-        <div className="container my-5 pb-3 px-md-5">
-            <div className="background-color-skyblue borderRadius">
-                <div style={{ height: '80px', backgroundColor: "#000000" }} className=" d-flex align-items-center justify-content-center col-12 mx-auto mb-3 text-center">
+        <div className="container position-relative my-5 pb-3">
+            <div className="bg-white ring-title ">
+                {/* <div style={{ height: '80px', backgroundColor: "#000000" }} className=" d-flex align-items-center  mb-3 text-center">
                     <h2 style={{ letterSpacing: '2px' }} className="text-light fw-bold text-uppercase fs-30">Our Top Selling Product</h2>
-                </div>
-                <div className="w-full position-relative">
-                    <div style={{ marginTop: "-30px" }} className="d-flex justify-content-left align-items-center ">
+                </div> */}
+                <div className="w-full shadow position-relative">
+                    <div style={{ marginTop: "45px" }} className="d-flex justify-content-left align-items-center ">
                         <Image
                             style={{ zIndex: "5" }}
                             src={`https://i.ibb.co/gPPFLNS/no1.png`}
@@ -27,10 +27,10 @@ const BestProduct = () => {
                             Best in Test 2023
                         </div>
                     </div>
-                    <div style={{ marginTop: "-30px" }} className="d-flex flex-column flex-md-row row justify-content-center px-3 align-items-center">
-                        <div className="col-12 col-md-3 bg-white border border-2">
+                    <div style={{ marginTop: "-50px" }} className="d-flex row justify-content-center align-items-center">
+                        <div className="col-12 col-md-3 bg-white">
                             <Image
-                                style={{ }}
+                                style={{}}
                                 src={`https://i.ibb.co/DzfHjH3/fba.png`}
                                 alt="amazon-fba-a-z-consultancy"
                                 title="amazon-fba-a-z-consultancy"
@@ -38,14 +38,50 @@ const BestProduct = () => {
                                 height={200}
                             />
                         </div>
-                        <div className="col-12 col-md-6 bg-white border">
-                            <h2 className="">hey there</h2>
+                        <div className="col-12 col-md-5 py-2 bg-white border">
+                            <h3 className="fs-20 pb-3 text-center">AMAZON FBA (A-Z) CONSULTANCY!</h3>
+                            <p className="fs-14 text-justify">{`Are you tired of struggling to make your Amazon business succeed? Let us help you take your business to the next level with our comprehensive Amazon FBA consultancy service package worth $3000. We'll handle everything from product and keyword research, and supplier sourcing to product manufacturing, packaging design, product listing, product photography, image design, shipment handling, and PPC management. Our professional team has nine years of experience in the Amazon FBA business, and we can reduce your workload and grow your brands on Amazon. Don't wait any longer, contact us today to take advantage of this unbeatable offer!`}</p>
                         </div>
-                        <div className="col-12 col-md-3 bg-gray border">
-                            <h2 className="">hey there</h2>
+                        <div className="col-12 col-md-3">
+                            <p className="pt-3 pb-2 fw-bold text-center m-0 lh-26">
+                                <span className="fs-40">$3000</span>
+                            </p>
+                            <p className="pb-5 fw-bold text-center m-0">
+                                <span className="fs-14">All Service Included</span>
+                            </p>
+                            <div className="d-flex justify-content-center">
+                                <Link href="/order" scroll>
+
+                                    <button
+                                        className="button px-4 py-1 slide_right mt-3"
+                                        style={{
+                                            padding: "5px 20px",
+                                            backgroundColor: "white",
+                                        }}
+                                    >
+                                        <h6 className="fs-14 d-inline">Order Now</h6>
+                                    </button>
+
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="ring d-none d-md-block">
+                <Image src={ring} alt="angle" />
+            </div>
+
+            <div className="angle d-none d-md-block">
+                <Image src={angle} alt="angle" />
+            </div>
+
+            <div className="ring-mobile-size d-md-none d-block">
+                <Image src={ring} height={50} width={50} alt="ring" />
+            </div>
+
+            <div className="angle-mobile-size d-md-none d-block">
+                <Image src={angle} height={50} width={50} alt="ring" />
             </div>
         </div>
     </>;
